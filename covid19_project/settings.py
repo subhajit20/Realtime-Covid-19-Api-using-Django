e15f5089b763b61cb6e8a6e81bd46b294f7400f2
Subhajit Ghosh <99127578+subhajit20@users.noreply.github.com>
2023-01-02 10:22:51 +0530

    Added extra route

--- covid19_project/settings.py
+++ covid19_project/settings.py
@@ -49,6 +49,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:9000",
     "http://127.0.0.1:5500",
     "http://127.0.0.1:3000",
+    "http://localhost:3000",
     "https://subhajit20.github.io/",
 ]
 
